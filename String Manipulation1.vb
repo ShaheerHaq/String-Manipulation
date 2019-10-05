7719b39baa05e1cc18e24f3764ea7817dee4f732
ShaheerHaq <55664901+ShaheerHaq@users.noreply.github.com>
2019-10-05 19:51:20 +0500

    Add files via upload

---
+++ String Manipulation1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim String1, String2 As String
+        Dim NotChar, NextChar As Char
+        Dim Count, NoOfChar As Integer
+
+        String1 = ""
+        String2 = ""
+        NotChar = ""
+        NextChar = ""
+        Count = 0
+        NoOfChar = 0
+
+        Console.Write("Enter the String: ")
+        String1 = Console.ReadLine
+        NoOfChar = Len(String1)
+
+        Console.Write("Enter which character to remove: ")
+        NotChar = Console.ReadLine
+
+        For Count = 1 To NoOfChar
+            NextChar = Mid(String1, Count, 1)
+            If NextChar <> NotChar Then
+                String2 = String2 & NextChar
+            End If
+        Next
+        Console.WriteLine("Your final text is: " & String2)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ String manipulation 2.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim String1, String2, FinalText, FirstWord, SecondWord As String
+        Dim SpacePos As Integer
+
+        String1 = ""
+        String2 = ""
+        FinalText = ""
+        FirstWord = ""
+        SecondWord = ""
+        SpacePos = 0
+
+        Console.Write("Enter Stiring 1: ")
+        String1 = Console.ReadLine
+        SpacePos = InStr(String1, " ")
+        FirstWord = Left(String1, SpacePos - 1)
+
+        Console.Write("Enter String 2: ")
+        String2 = Console.ReadLine
+        SpacePos = InStr(String2, " ")
+        SecondWord = Right(String2, Len(String2) - SpacePos)
+
+        FinalText = FirstWord & " " & SecondWord
+        Console.WriteLine("Your final text is: " & FinalText)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
