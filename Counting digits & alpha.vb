c739a33ecf7e3ee45f77346c897c9ab67e11992b
ShaheerHaq <55664901+ShaheerHaq@users.noreply.github.com>
2019-10-11 20:43:53 +0500

    Counting no. of digits & alphabets
    Counting the digits and alphabets in a particular string

---
+++ Counting digits & alpha.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim String1 As String
+        Dim NextChar As Char
+        Dim Count, Digit, Alpha As Integer
+
+        String1 = ""
+        NextChar = ""
+        Count = 0
+        Digit = 0
+        Alpha = 0
+
+        Console.Write("Enter the string: ")
+        String1 = Console.ReadLine
+        For Count = 1 To Len(String1)
+            NextChar = Mid(String1, Count, 1)
+            If NextChar >= "0" And NextChar <= "9" Then
+                Digit = Digit + 1
+            Else
+                Alpha = Alpha + 1
+            End If
+        Next
+        Console.WriteLine("No. of digits: " & Digit)
+        Console.WriteLine("No. of alphabets: " & Alpha)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
