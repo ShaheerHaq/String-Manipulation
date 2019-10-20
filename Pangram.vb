79985ab6208f657075f35bcc4de1fd597b22a021
ShaheerHaq <55664901+ShaheerHaq@users.noreply.github.com>
2019-10-20 22:05:20 +0500

    Checking pangram
    Verification of a string whether it is a pangram or not

---
+++ Pangram.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim String1, Alphabets As String
+        Dim Count As Integer
+        Dim IsPangram As Boolean
+        Dim NextChar As Char
+
+        String1 = ""
+        Alphabets = "abcdefghijklmnopqrstuvwxyz"
+        Count = 0
+        IsPangram = True
+        NextChar = ""
+
+        Console.Write("Enter the string: ")
+        String1 = Console.ReadLine
+        String1 = LCase(String1)
+
+        For Count = 1 To Len(Alphabets)
+            NextChar = Mid(Alphabets, Count, 1)
+            If InStr(String1, NextChar) = 0 Then
+                IsPangram = False
+            End If
+        Next
+        If IsPangram = True Then
+            Console.Write("It is a pangram")
+        ElseIf IsPangram = False Then
+            Console.Write("It is not a pangram")
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
