d6f8146a900091ae9175804563a8cafc7b497d2a
ShaheerHaq <55664901+ShaheerHaq@users.noreply.github.com>
2019-10-20 21:40:00 +0500

    Counting vowels
    Counting the no. of vowels in a particular string

---
+++ VowelsCount.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim String1, Vowels As String
+        Dim NextChar, VChar As String
+        Dim Count, Vcount, VowelsCount As Integer
+
+        String1 = ""
+        Vowels = "AEIOUaeiou"
+        NextChar = ""
+        VChar = ""
+        Count = 0
+        Vcount = 0
+        VowelsCount = 0
+
+        Console.Write("Enter the string: ")
+        String1 = Console.ReadLine
+
+        For Count = 1 To Len(String1)
+            NextChar = Mid(String1, Count, 1)
+            For Vcount = 1 To Len(Vowels)
+                VChar = Mid(Vowels, Vcount, 1)
+                If VChar = NextChar Then
+                    VowelsCount = VowelsCount + 1
+                End If
+            Next
+        Next
+        Console.Write("No. of vowels in your string are: " & VowelsCount)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
