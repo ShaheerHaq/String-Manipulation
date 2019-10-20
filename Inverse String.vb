8cc97e42eeaaacb1d8e9ae21fbdd3eaf0c8c5a93
ShaheerHaq <55664901+ShaheerHaq@users.noreply.github.com>
2019-10-20 21:43:45 +0500

    Inversing a string
    Reversing all the characters in a string

---
+++ Inverse String.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim String1 As String
+        Dim String2 As String
+        Dim NextChar As Char
+        Dim Count As Integer
+
+        String1 = ""
+        String2 = ""
+        NextChar = ""
+        Count = 0
+
+        Console.Write("Enter the string: ")
+        String1 = Console.ReadLine
+
+        For Count = Len(String1) To 1 Step -1
+            NextChar = Mid(String1, Count, 1)
+            String2 = String2 & NextChar
+        Next
+        Console.Write("Your inversed string is " & String2)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
